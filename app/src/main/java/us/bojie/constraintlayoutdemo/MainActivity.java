c0b0f5152b047465189536c8e0b75dd36c4270b4
Bojie Jiang <jiangbojie@gmail.com>
2017-05-15 20:47:14 -0700

    change btn in code

--- app/src/main/java/us/bojie/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/us/bojie/constraintlayoutdemo/MainActivity.java
@@ -1,13 +1,35 @@
 package us.bojie.constraintlayoutdemo;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintSet mConstraintSet1;
+    private ConstraintSet mConstraintSet2;
+    private ConstraintLayout mConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.root);
+        mConstraintSet1 = new ConstraintSet();
+        mConstraintSet2 = new ConstraintSet();
+
+        mConstraintSet1.clone(mConstraintLayout);
+        mConstraintSet2.clone(mConstraintLayout);
+    }
+
+    public void reset(View view) {
+        mConstraintSet1.applyTo(mConstraintLayout);
+    }
+
+    public void change(View view) {
+        mConstraintSet2.setMargin(R.id.button1, ConstraintSet.START, 30);
+        mConstraintSet2.applyTo(mConstraintLayout);
     }
 }
