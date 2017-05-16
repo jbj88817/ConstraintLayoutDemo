b667375aa45d29b65fa100489617eb73561ffff8
Bojie Jiang <jiangbojie@gmail.com>
2017-05-15 21:09:43 -0700

    Applying chain in code

--- app/src/main/java/us/bojie/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/us/bojie/constraintlayoutdemo/MainActivity.java
@@ -32,7 +32,42 @@ public class MainActivity extends AppCompatActivity {
 
     public void change(View view) {
         TransitionManager.beginDelayedTransition(mConstraintLayout);
-        mConstraintSet2.setMargin(R.id.button1, ConstraintSet.START, 30);
+//        mConstraintSet2.setMargin(R.id.button1, ConstraintSet.LEFT, 30);
+//        mConstraintSet2.setMargin(R.id.button1, ConstraintSet.LEFT, 0);
+//        mConstraintSet2.centerHorizontally(R.id.button1, R.id.root);
+//        mConstraintSet2.connect(R.id.button1, ConstraintSet.LEFT, R.id.root, ConstraintSet.LEFT, 50);
+
+        mConstraintSet2.clear(R.id.button1);
+        mConstraintSet2.clear(R.id.button2);
+        mConstraintSet2.clear(R.id.button3);
+
+        mConstraintSet2.connect(R.id.button1, ConstraintSet.LEFT, R.id.root, ConstraintSet.LEFT, 0);
+        mConstraintSet2.connect(R.id.button3, ConstraintSet.RIGHT, R.id.root, ConstraintSet.RIGHT, 0);
+
+        mConstraintSet2.connect(R.id.button2, ConstraintSet.LEFT, R.id.button1, ConstraintSet.RIGHT, 0);
+        mConstraintSet2.connect(R.id.button2, ConstraintSet.RIGHT, R.id.button3, ConstraintSet.LEFT, 0);
+
+        mConstraintSet2.connect(R.id.button1, ConstraintSet.RIGHT, R.id.button2, ConstraintSet.LEFT, 0);
+        mConstraintSet2.connect(R.id.button3, ConstraintSet.LEFT, R.id.button2, ConstraintSet.RIGHT, 0);
+
+        mConstraintSet2.createHorizontalChain(
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.LEFT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.RIGHT,
+                new int[]{R.id.button1, R.id.button2, R.id.button3},
+                null,
+                ConstraintSet.CHAIN_SPREAD
+        );
+
+        mConstraintSet2.constrainHeight(R.id.button1,ConstraintSet.WRAP_CONTENT);
+        mConstraintSet2.constrainHeight(R.id.button2,ConstraintSet.WRAP_CONTENT);
+        mConstraintSet2.constrainHeight(R.id.button3,ConstraintSet.WRAP_CONTENT);
+
+        mConstraintSet2.constrainWidth(R.id.button1,ConstraintSet.WRAP_CONTENT);
+        mConstraintSet2.constrainWidth(R.id.button2,ConstraintSet.WRAP_CONTENT);
+        mConstraintSet2.constrainWidth(R.id.button3,ConstraintSet.WRAP_CONTENT);
+
         mConstraintSet2.applyTo(mConstraintLayout);
     }
 }
